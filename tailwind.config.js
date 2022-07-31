8b752833fbb7300170cae450e8c72a5d4ebc893c
octopusknight <audroid16@gmail.com>
2022-07-31 12:21:56 -0400

    chore: update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./resources/**/*.{html,js,php}", "./index.php"],
+  content: [
+    "./resources/**/*.{html,js,php}",
+    "./index.php",
+    "./guides/*.{html,js,php}"
+  ],
   theme: {
     extend: {},
   },
