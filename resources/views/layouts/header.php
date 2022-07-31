e945c6211791005915b5ca7f9111a1d69ae96c9d
octopusknight <audroid16@gmail.com>
2022-07-31 12:25:28 -0400

    refactor header

--- resources/views/layouts/header.php
+++ resources/views/layouts/header.php
@@ -1,8 +1,10 @@
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="css/app.css" rel="stylesheet">
-  <!-- <script src="js/app.js" defer></script> -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/css/app.css" rel="stylesheet">
+    <script src="/js/app.js" defer></script>
+    <title>Reservoir</title>
 </head>
